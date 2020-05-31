3d9339914f690afe7aed63b5fb2776167919c6a5
daniel-snowsurf <daniel.snowsurf@gmail.com>
2020-05-31 18:58:14 +0200

    add extra switches no stock, requires removing standard GUI

--- src/target/tx/devo/devo7e/hardware.h
+++ src/target/tx/devo/devo7e/hardware.h
@@ -2,18 +2,7 @@
 #define _DEVO7E_HARDWARE_H_
 
 #include "target/drivers/mcu/stm32/gpio.h"
-
-#define BUTTON_MATRIX { \
-/*         C.6              C.7              C.8              C.9          */  \
-/*B.5*/    BUT_TRIM_RH_POS, BUT_TRIM_RH_NEG, BUT_TRIM_RV_POS, BUT_TRIM_RV_NEG, \
-/*B.6*/    SW_10,           BUT_ENTER,       BUT_RIGHT,       BUT_LEFT,        \
-/*B.7*/    BUT_TRIM_LV_POS, BUT_TRIM_LV_NEG, BUT_TRIM_LH_NEG, BUT_TRIM_LH_POS, \
-/*B.8*/    SW_09,           BUT_DOWN,        BUT_UP,          BUT_EXIT,        \
-    }
-
-#define BUTTON_MATRIX_ROW_OD ((struct mcu_pin){GPIOB, GPIO5 | GPIO6 | GPIO7 | GPIO8})
-#define BUTTON_MATRIX_COL_PU ((struct mcu_pin){GPIOC, GPIO6 | GPIO7 | GPIO8 | GPIO9})
-#define EXTRA_SWITCH_COL_OD ((struct mcu_pin){GPIOC, GPIO6})
+#include "../common/hardware_t8sg_buttonmatrix.h"
 
 // Analog inputs must be in same order as in CHANDEF
 #define ADC_CHANNELS { \
@@ -21,6 +10,8 @@
     ADC_CHAN(GPIOC, GPIO1, CHAN_NONINV),  /* ADC123_11 - INP_ELE */ \
     ADC_CHAN(GPIOC, GPIO0, CHAN_INVERT),  /* ADC123_10 - INP_THR */ \
     ADC_CHAN(GPIOC, GPIO3, CHAN_INVERT),  /* ADC123_13 - INP_RUD */ \
+    ADC_CHAN(GPIOA, GPIO0, CHAN_NONINV),  /* ADC123_0  - INP_AUX4 */ \
+    ADC_CHAN(GPIOA, GPIO4, CHAN_NONINV),  /* ADC12_4   - INP_AUX5 */ \
     ADC_CHAN(0, 16, CHAN_NONINV),         /* TEMPERATURE */ \
     ADC_CHAN(GPIOC, GPIO4, CHAN_NONINV),  /* ADC12_14  */ \
     }
@@ -32,21 +23,23 @@
 #define EXTRA_SWITCHES \
     EXTRA_3WAY(INP_SWA, 0x04, 0x08, CHAN_NONINV, SWITCH_3x1) \
     EXTRA_3WAY(INP_SWB, 0x01, 0x02, CHAN_NONINV, SWITCH_3x2) \
-    EXTRA_2WAY(INP_SWA, 0x04, 0x08, CHAN_INVERT, SWITCH_2x1) \
-    EXTRA_2WAY(INP_SWB, 0x01, 0x02, CHAN_INVERT, SWITCH_2x2)
+    EXTRA_2WAY(INP_SWG, 0x04, 0x08, CHAN_INVERT, SWITCH_2x2) \
+    EXTRA_2WAY(INP_SWH, 0x01, 0x02, CHAN_INVERT, SWITCH_2x1)
 
-#define ADDON_SWITCH_CFG \
-    ADDON_SWITCH("2x1", SWITCH_2x1, 0) \
-    ADDON_SWITCH("2x2", SWITCH_2x2, 0) \
-    ADDON_SWITCH("3x1", SWITCH_3x1, 0) \
-    ADDON_SWITCH("3x2", SWITCH_3x2, 0)
+#define REPLACEMENT_SWITCHES \
+    EXTRA_3WAY(INP_SWA, CH(23), CH(24), CHAN_NONINV, SWITCH_3x1) \
+    EXTRA_3WAY(INP_SWB, CH(25), CH(26), CHAN_NONINV, SWITCH_3x2) \
+    EXTRA_3WAY(INP_SWC, CH(27), CH(28), CHAN_NONINV, SWITCH_3x3) \
+    EXTRA_3WAY(INP_SWD, CH(29), CH(30), CHAN_NONINV, SWITCH_3x4) \
+    EXTRA_2WAY(INP_SWA, CH(25), CH(25), CHAN_NONINV, SWITCH_2x8) \
+    EXTRA_2WAY(INP_SWB, CH(26), CH(26), CHAN_NONINV, SWITCH_2x7) \
+    EXTRA_2WAY(INP_SWC, CH(27), CH(27), CHAN_NONINV, SWITCH_2x6) \
+    EXTRA_2WAY(INP_SWD, CH(28), CH(28), CHAN_NONINV, SWITCH_2x5) \
+    EXTRA_2WAY(INP_SWE, CH(29), CH(29), CHAN_NONINV, SWITCH_2x4) \
+    EXTRA_2WAY(INP_SWF, CH(30), CH(30), CHAN_NONINV, SWITCH_2x3) \
+    EXTRA_2WAY(INP_SWG, CH(31), CH(31), CHAN_NONINV, SWITCH_2x2) \
+    EXTRA_2WAY(INP_SWH, CH(32), CH(32), CHAN_NONINV, SWITCH_2x1) \
 
-#define SWITCH_3x2  ((1 << INP_SWA0) | (1 << INP_SWA1) | (1 << INP_SWA2) \
-                   | (1 << INP_SWB0) | (1 << INP_SWB1) | (1 << INP_SWB2))
-#define SWITCH_3x1  ((1 << INP_SWA0) | (1 << INP_SWA1) | (1 << INP_SWA2))
-#define SWITCH_2x2  ((1 << INP_SWA0) | (1 << INP_SWA1) \
-                   | (1 << INP_SWB0) | (1 << INP_SWB1))
-#define SWITCH_2x1  ((1 << INP_SWA0) | (1 << INP_SWA1))
-#define SWITCH_STOCK ((1 << INP_HOLD0) | (1 << INP_HOLD1) \
-                    | (1 << INP_FMOD0) | (1 << INP_FMOD1))
+#include "target/tx/devo/common/extra_switch_7e.h"
+
 #endif  // _DEVO7E_HARDWARE_H_
--- src/target/tx/devo/devo7e/target_defs.h
+++ src/target/tx/devo/devo7e/target_defs.h
@@ -6,26 +6,22 @@
 #define SPIFLASH_SECTOR_OFFSET 0
 #define SPIFLASH_SECTORS 512
 
-#if defined BUILDTYPE_DEV && ! defined EMULATOR
-//No room for debug and standard gui
- #define HAS_STANDARD_GUI   0
-#else
- #define HAS_STANDARD_GUI   1
-#endif
-
+#define HAS_STANDARD_GUI   0
 #define HAS_ADVANCED_GUI    1
-#define HAS_PERMANENT_TIMER 0
+#define HAS_PERMANENT_TIMER 1
 #define HAS_TELEMETRY       1
 #define HAS_EXTENDED_TELEMETRY 0
 #define HAS_TOUCH           0
 #define HAS_RTC             0
 #define HAS_VIBRATINGMOTOR  1
-#define HAS_DATALOG         0
+#define HAS_DATALOG         1
 #define SUPPORT_SCANNER     0
-#define HAS_LAYOUT_EDITOR   0
+#define HAS_LAYOUT_EDITOR   1
 #define HAS_EXTRA_SWITCHES  OPTIONAL
+#define HAS_SWITCHES_NOSTOCK 1
 #define HAS_EXTRA_BUTTONS   0
 #define HAS_BUTTON_MATRIX_PULLUP 1
+#define HAS_EXTRA_POTS      OPTIONAL
 #define HAS_MULTIMOD_SUPPORT 1
 #define HAS_VIDEO           0
 #define HAS_4IN1_FLASH      0
@@ -52,7 +48,11 @@
 #define MAX_POINTS 13
 #define NUM_MIXERS ((NUM_OUT_CHANNELS + NUM_VIRT_CHANNELS) * 4)
 
-#define INP_HAS_CALIBRATION 4
+#if HAS_EXTRA_POTS
+  #define INP_HAS_CALIBRATION 6
+#else
+  #define INP_HAS_CALIBRATION 4
+#endif
 
 /* Compute voltage from y = 2.1592x + 0.2493 */
 #define VOLTAGE_NUMERATOR 216
